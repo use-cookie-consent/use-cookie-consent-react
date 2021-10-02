89461823b6bdbc97c01e0ae323b94671c859cea8
Antoni Silvestrovič <antoni.silvestrovic@gmail.com>
2021-10-02 20:09:25 +0300

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.tsx"],
+  // Add any Storybook addons you want here: https://storybook.js.org/addons/
+  addons: [],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]],
+      },
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    return config;
+  },
+};
---
+++ src/test/index.test.ts
@@ -0,0 +1,5 @@
+describe("testing jest", () => {
+  it("should pass guaranteed test", () => {
+    expect(true).toBeTruthy();
+  });
+});
