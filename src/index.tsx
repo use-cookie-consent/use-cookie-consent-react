033e4650e74f38745361c1f8cc2c71781958c445
Antoni Silvestrovič <antoni.silvestrovic@gmail.com>
2021-10-12 12:07:23 +0300

    Added base code provided by @pixelass

---
+++ src/index.tsx
@@ -0,0 +1,80 @@
+import React, { createContext, FC, useContext, useMemo } from "react";
+import { CookieConsentHookState, useCookieConsent } from "use-cookie-consent";
+
+export const CookieConsentContext = createContext<CookieConsentHookState>({
+    consent: {},
+    acceptCookies() {
+        /**/
+    },
+    declineAllCookies() {
+        /**/
+    },
+    acceptAllCookies() {
+        /**/
+    },
+    didAcceptAll() {
+        return false;
+    },
+    didDeclineAll() {
+        return false;
+    },
+    cookies: {
+        set() {
+            return undefined;
+        },
+        get() {
+            return "";
+        },
+        getAll() {
+            return {};
+        },
+        getJSON() {
+            /**/
+        },
+        getAllJSON() {
+            return {};
+        },
+        remove() {
+            /**/
+        },
+    },
+});
+
+export const CookieConsentProvider: FC = ({ children }) => {
+    const {
+        consent,
+        acceptAllCookies,
+        declineAllCookies,
+        acceptCookies,
+        didAcceptAll,
+        didDeclineAll,
+        cookies,
+    } = useCookieConsent();
+
+    const context = useMemo(
+        () => ({
+            consent,
+            acceptAllCookies,
+            declineAllCookies,
+            acceptCookies,
+            didAcceptAll,
+            didDeclineAll,
+            cookies,
+        }),
+        [
+            consent,
+            acceptAllCookies,
+            declineAllCookies,
+            acceptCookies,
+            didAcceptAll,
+            didDeclineAll,
+            cookies,
+        ]
+    );
+
+    return (
+        <CookieConsentContext.Provider value={context} > {children} < /CookieConsentContext.Provider>
+            );
+};
+
+export const useCookieConsentContext = () => useContext(CookieConsentContext);
